f3d96225ec40d44407f4c5c97f1d7037a0e30202
Екатерина <sancy95@mail.ru>
2019-04-29 13:37:53 +0700

    Fixed bugs

--- src/com/katee/BusinessService/UserService.kts
+++ src/com/katee/BusinessService/UserService.kts
@@ -6,12 +6,12 @@ class UserService {
     /*Передаю логин и список List<User>.
     Внутри функции UserByLogin перебираю коллекцию и сравниваю каждую запись с логином,
     чтобы найти нужную из коллекции*/
-    fun UserByLogin(val login:String, users: List<User>): User? {
+    fun UserByLogin(login:String, users: List<User>): User? {
         val find = users.find { it.login == login }
         return find
     }
     //Внутри функции ValidatePass проверяю есть ли получавшийся пароль в базе данных
-    fun ValidatePass(val user: User, val pass: String): Boolean {
+    fun ValidatePass(user: User, val pass: String): Boolean {
         val hashCode = hash("""$pass${user.salt}""")
         val comparison = user.pass == hashCode
         return comparison
